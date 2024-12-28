9fe49cd2734a237d169884daf0bb404915113477
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2024-12-29 02:54:51 +0300

    feat: Added ValidationFilterAttribute class to ActionFilters in the presentation layer. The ValidationFilterAttribute class was implemented to improve request validation by checking DTO objects and model state before executing actions.

--- Presentation/ActionFilters/ValidationFilterAttribute.cs
+++ Presentation/ActionFilters/ValidationFilterAttribute.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace Presentation.ActionFilters
 {
-    internal class ValidationFilterAttribute
+    public class ValidationFilterAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var controller = context.RouteData.Values["controller"];
+            var action = context.RouteData.Values["action"];
+
+            var param = context.ActionArguments
+                .SingleOrDefault(p => p.Value.ToString().Contains("Dto")).Value;
+
+            if (param == null)
+            {
+                context.Result = new BadRequestObjectResult($"Object is null. Controller: {controller}, Action: {action}");
+                return;
+            }
+
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+            }
+        }
     }
 }
