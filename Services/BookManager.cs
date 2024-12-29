ab0f2217602a64ec7f13fdfa922e300fce2202e7
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2024-12-29 04:41:11 +0300

    feat: Added the GetOneBookByIdAndCheckExists method to BookManager. Implemented the GetOneBookByIdAndCheckExists method to centralize the logic for fetching a book by ID and handling the book not found exception. The method was implemented in Create, Update and Delete operations.

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -37,12 +37,7 @@ namespace Services
 
         public async Task DeleteAsync(int id, bool trackChanges)
         {
-            //check entity
-            var entity = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
-            if (entity == null)
-            {
-                throw new BookNotFoundException(id);
-            }
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
             _manager.Book.DeleteOneBook(entity);
             await _manager.SaveAsync();
         }
@@ -55,28 +50,28 @@ namespace Services
 
         public async Task<BookDto> GetOneBookByIdAsync(int id, bool trackChanges)
         {
-            var book = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
-            if (book is null)
-            {
-                throw new BookNotFoundException(id);
-            }
+            var book = await GetOneBookByIdAndCheckExists(id, trackChanges);
             return _mapper.Map<BookDto>(book);
         }
 
         public async Task UpdateOneBookAsync(int id, BookDtoForUpdate bookDto, bool trackChanges)
         {
-            //check entity
-            var entity = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
-            if (entity is null)
-            {
-                throw new BookNotFoundException(id);
-            }
-
+            var entity = await GetOneBookByIdAndCheckExists(id, trackChanges);
             entity = _mapper.Map<Book>(bookDto);
-
             _manager.Book.Update(entity);
             await _manager.SaveAsync();
         }
+
+        private async Task<Book> GetOneBookByIdAndCheckExists(int id, bool trackChanges)
+        {
+            var entity = await _manager.Book.GetOneBookByIdAsync(id, trackChanges);
+            if (entity == null)
+            {
+                throw new BookNotFoundException(id);
+            }
+            return entity;
+        }
     }
+
 
 }
