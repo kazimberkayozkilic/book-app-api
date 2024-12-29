e62dafee5437e77fc50a9a7078edabac0749c28b
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2024-12-29 04:02:00 +0300

    feat: implemented LogFilterAttribute class to log request details. Added LogFilterAttribute class to log request details including model name, controller, action and id using ILoggerService. Logs are generated during action execution for better monitoring and tracking.

--- Presentation/ActionFilters/LogFilterAttribute.cs
+++ Presentation/ActionFilters/LogFilterAttribute.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Entities.LogModel;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,35 @@ using System.Threading.Tasks;
 
 namespace Presentation.ActionFilters
 {
-    public class LogFilterAttribute
+    public class LogFilterAttribute : ActionFilterAttribute
     {
+        private readonly ILoggerService _logger;
 
+        public LogFilterAttribute(ILoggerService logger)
+        {
+            _logger = logger;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            _logger.LogInfo(Log("OnActionExecuting", context.RouteData));
+        }
+
+        private string Log(string ModelName, RouteData routeData)
+        {
+            var LogDetails = new LogDetails()
+            {
+                ModelModel = ModelName,
+                Controller = routeData.Values["controller"],
+                Action = routeData.Values["action"]
+            };
+
+            if (routeData.Values.Count >= 3)
+            {
+                LogDetails.Id = routeData.Values["Id"];
+
+            }
+            return LogDetails.ToString();
+        }
     }
 }
