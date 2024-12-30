b3d5ab8c7f0ecf77911209bbc9de4c8b021a7e41
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2024-12-30 20:19:50 +0300

    feat: Implemented pagination in BookRepository with BookParameters. Added pagination logic to the GetAllBooksAsync method in BookRepository. Introduced BookParameters parameter to manage page number and page size, and implemented Skip and Get methods for efficient data retrieval.

--- Repositories/EFCore/BookRepository.cs
+++ Repositories/EFCore/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
 using System;
@@ -20,7 +21,7 @@ namespace Repositories.EFCore
 
         public void DeleteOneBook(Book book) => Delete(book);
 
-        public async Task<IEnumerable<Book>> GetAllBooksAsync(bool trackChanges) => await FindAll(trackChanges).OrderBy(b => b.Id).ToListAsync();
+        public async Task<IEnumerable<Book>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges) => await FindAll(trackChanges).OrderBy(b => b.Id).Skip((bookParameters.PageNumber-1) * bookParameters.PageSize).Take(bookParameters.PageSize).ToListAsync();
 
         public async Task<Book> GetOneBookByIdAsync(int id, bool trackChanges) => await FindByCondition(b => b.Id == id, trackChanges).SingleOrDefaultAsync();
 
