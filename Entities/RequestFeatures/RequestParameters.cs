e5b1df7fc09c12b0807cd353d9097414efb0f989
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2024-12-30 19:43:26 +0300

    feat: added RequestParameters and BookParameters classes for pagination. To manage pagination, the RequestParameters abstract class with PageNumber and PageSize properties was implemented, and PageSize was limited to a maximum of 50. For book-specific pagination parameters, the BookParameters class was created under RequestFeatures.

--- Entities/RequestFeatures/RequestParameters.cs
+++ Entities/RequestFeatures/RequestParameters.cs
@@ -8,6 +8,20 @@ namespace Entities.RequestFeatures
 {
     public abstract class RequestParameters
     {
+        const int maxPageSize = 50;
+        public int PageNumber { get; set; }
+        private int _pageSize;
 
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
     }
 }
