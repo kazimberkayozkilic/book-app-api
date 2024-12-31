53010663c8294aae5ca773888b31addf53a58ee3
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2024-12-31 20:58:22 +0300

    feat: added ToPagedList method to PagedList class for paging. Added a static ToPagedList method to the PagedList<T> class under RequestFeatures in the Entities layer. This method creates a PagedList instance from an IEnumerable resource, applying pagination logic using the provided page number and page size.

--- Entities/RequestFeatures/PagedList.cs
+++ Entities/RequestFeatures/PagedList.cs
@@ -20,5 +20,12 @@ namespace Entities.RequestFeatures
             };
             AddRange(items);
         }
+        public static PagedList<T> ToPagedList(IEnumerable<T> source, 
+            int pageNumber, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, count, pageNumber, pageSize);
+        }
     }
 }
