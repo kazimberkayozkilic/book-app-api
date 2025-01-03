d2aba91860152d853dfc869f67b9d6b53405cabd
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-04 02:50:09 +0300

    Refactor Integrated OrderQueryBuilder into Sort method for dynamic sorting. - Updated the Sort method in BookRepositoryExtensions to use OrderQueryBuilder.CreateOrderQuery for cleaner dynamic query generation. - Implemented CreateOrderQuery<T> in OrderQueryBuilder with logic to parse and build order queries from a query string.

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -31,34 +31,14 @@ namespace Repositories.EFCore.Extensions
                 return books.OrderBy(book => book.Id);
             }
 
-            var orderParams = orderByQueryString.Trim().Split(',');
-            var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book>(orderByQueryString);
 
-            var orderQueryBuilder = new StringBuilder();
-
-            foreach (var param in orderParams)
-            {
-                if (string.IsNullOrEmpty(param)) continue;
-                var propertyFromQueryName = param.Split(' ')[0];
-                var objectProperty = propertyInfos
-                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
-                    StringComparison.InvariantCultureIgnoreCase));
-
-                if (objectProperty is null) continue;
-
-                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
-
-                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
-
-            }
-
-            var OrderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
-            if (OrderQuery is null)
+            if (orderQuery is null)
             {
                 return books.OrderBy(book => book.Id);
             }
 
-            return books.OrderBy(OrderQuery);
+            return books.OrderBy(orderQuery);
 
         }
     }
