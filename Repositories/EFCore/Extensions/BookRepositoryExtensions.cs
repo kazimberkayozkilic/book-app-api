8f688eed1954c9a58f53d9188e5e3acf37acce47
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-04 00:26:25 +0300

    feat: Added dynamic sorting to BookRepositoryExtensions. - Added `Sort` method to `BookRepositoryExtensions` for dynamic sorting of books. - Installed `System.Linq.Dynamic.Core` package to support dynamic LINQ queries. - Set the default sort to `Id` if a valid `orderByQueryString` is not provided.

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
 
 namespace Repositories.EFCore.Extensions
 {
@@ -20,6 +22,44 @@ namespace Repositories.EFCore.Extensions
 
             var lowerCaseSearchTerm = searchTerm.Trim().ToLower();
             return books.Where(book => book.Title.ToLower().Contains(lowerCaseSearchTerm));
+        }
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+            {
+                return books.OrderBy(book => book.Id);
+            }
+
+            var orderParams = orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(Book).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrEmpty(param)) continue;
+                var propertyFromQueryName = param.Split(' ')[0];
+                var objectProperty = propertyInfos
+                    .FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName,
+                    StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty is null) continue;
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction}, ");
+
+            }
+
+            var OrderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+            if (OrderQuery is null)
+            {
+                return books.OrderBy(book => book.Id);
+            }
+
+            return books.OrderBy(OrderQuery);
+
         }
     }
 }
