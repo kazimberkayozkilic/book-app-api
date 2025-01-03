4f1e31d736d46c6e8c0ccc638c4bf5ea0205b9ce
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-03 18:45:49 +0300

    refactor: add BookRepositoryExtensions for filtering logic and updated BookRepository. - Created `BookRepositoryExtensions` with `FilterBooks` method to encapsulate the filtering logic. - Updated `BookRepository`:   - Marked `BookRepository` class as `sealed` to avoid inheritance.   - Refactored the filtering logic in `GetAllBooksAsync` to use the `FilterBooks` extension method for cleaner code.

---
+++ Repositories/EFCore/BookRepositoryExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public static class BookRepositoryExtensions
+    {
+        public static IQueryable<Book> FilterBooks(this IQueryable<Book> books, 
+            uint MinPrice, uint MaxPrice) =>
+            books.Where(book => book.Price >= MinPrice && book.Price <= MaxPrice);
+    }
+}
