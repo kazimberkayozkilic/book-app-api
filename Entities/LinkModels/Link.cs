84615c0146c1e245e1447c8879977c82e60dd6f6
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-06 19:53:20 +0300

    refactor LinkResourceBase class extracted to its own file. - The `LinkResourceBase` class has been separated from the `Link` class.

--- Entities/LinkModels/Link.cs
+++ Entities/LinkModels/Link.cs
@@ -24,13 +24,4 @@ namespace Entities.LinkModels
         }
 
     }
-
-    public class LinkResourceBase
-    {
-        public LinkResourceBase()
-        {
-
-        }
-        public List<Link> Links { get; set; }
-    }
 }
