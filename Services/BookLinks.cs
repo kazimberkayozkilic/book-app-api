20e049a822b8cf98b7bab73da7fcb7db47b0668e
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-08 19:27:06 +0300

    feat: Create the BookLinks class and implement the IBookLinks interface. - Created the `BookLinks` class in the `Services` namespace to implement the `IBookLinks` interface. - Added `TryGenerateLinks` method, which currently throws a `NotImplementedException`.

---
+++ Services/BookLinks.cs
@@ -0,0 +1,20 @@
+﻿using Entities.DataTransferObjects;
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Http;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class BookLinks : IBookLinks
+    {
+        public LinkResponse TryGenerateLinks(IEnumerable<BookDto> booksDto, string fields, HttpContext HttpContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
