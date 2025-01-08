271625f11631a60988047c6862851cd183ee9cae
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-08 19:09:36 +0300

    feat: Implemented ValidateMediaTypeAttribute for Accept header validation. - Improved the `ValidateMediaTypeAttribute` class in the `Presentation.ActionFilters` namespace. - Added `OnActionExecuting` method to validate the presence of `Accept` header in incoming requests. - Added logic to parse and validate the media type of the `Accept` header.

--- Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
+++ Presentation/ActionFilters/ValidateMediaTypeAttribute.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,25 @@ namespace Presentation.ActionFilters
 {
     public class ValidateMediaTypeAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var accepHeaderPresent = context.HttpContext.Request.Headers.ContainsKey("Accept");
+
+            if (!accepHeaderPresent)
+            {
+                context.Result = new BadRequestObjectResult($"Accept header not present!");
+                return;
+            }
+
+            var mediaType = context.HttpContext.Request.Headers["Accept"].FirstOrDefault();
+
+            if(MediaTypeHeaderValue.TryParse(mediaType, out MediaTypeHeaderValue? outMediaType))
+            {
+                context.Result = new BadRequestObjectResult($"Media type not supported! {outMediaType}");
+                return;
+            }
+
+            context.HttpContext.Items.Add("AcceptHeader", outMediaType);
+        }
     }
 }
