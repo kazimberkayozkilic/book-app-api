932b497414ba57257cebf25eb6b1b7f4f7084b7c
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-08 20:22:51 +0300

    feat: Implemented the TryGenerateLinks method in the BookLinks class. - Implemented `TryGenerateLinks' method to generate shaped data or links based on the “Accept” header. - Added logic to conditionally generate HATEOAS links for book entities. - Integrated `IDataShaper<BookDto>` and `LinkGenerator` for dynamic data shaping and link creation.

--- Entities/LinkModels/LinkCollectionWrapper.cs
+++ Entities/LinkModels/LinkCollectionWrapper.cs
@@ -1,6 +1,12 @@
-﻿namespace Entities.LinkModels
+﻿using Entities.Models;
+
+namespace Entities.LinkModels
 {
     public class LinkCollectionWrapper<T>
     {
+        public void AddRange(List<Entity> shapedBooks)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Services/BookLinks.cs
+++ Services/BookLinks.cs
@@ -1,7 +1,9 @@
 ﻿using Entities.DataTransferObjects;
 using Entities.LinkModels;
+using Entities.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Net.Http.Headers;
 using Services.Contracts;
 using System;
 using System.Collections.Generic;
@@ -24,7 +26,53 @@ namespace Services
 
         public LinkResponse TryGenerateLinks(IEnumerable<BookDto> booksDto, string fields, HttpContext HttpContext)
         {
-            throw new NotImplementedException();
+            var shapedBooks = ShapedData(booksDto, fields);
+            if (ShouldGenerateLinks(HttpContext))
+            {
+                return ReturnLinkedBooks(booksDto, fields, HttpContext, shapedBooks);
+            }
+            return ReturnShapedBooks(shapedBooks);
         }
+
+        private LinkResponse ReturnLinkedBooks(IEnumerable<BookDto> booksDto, string fields, HttpContext httpContext, List<Entity> shapedBooks)
+        {
+            var bookDtoList = booksDto.ToList();
+            for (int index = 0; index < bookDtoList.Count; index++)
+            {
+                var bookLinks = CreateForBook(httpContext, bookDtoList[index].Id, fields);
+                shapedBooks[index].Add("Links", bookLinks);
+            }
+            var bookCollection = new LinkCollectionWrapper<Entity>();
+            bookCollection.AddRange(shapedBooks);
+            return new LinkResponse { HasLinks = true, LinkedEntities = bookCollection };
+        }
+
+        private List<Link> CreateForBook(HttpContext httpContext, int ıd, string fields)
+        {
+            var links = new List<Link>()
+            {
+                new Link("a1","b1","c1"),
+                new Link("a2","b2","c2")
+            };
+            return links;
+        }
+
+        private LinkResponse ReturnShapedBooks(List<Entity> shapedBooks)
+        {
+            return new LinkResponse() { ShapedEntities = shapedBooks };
+        }
+
+        private bool ShouldGenerateLinks(HttpContext httpContext)
+        {
+            var mediaType = (MediaTypeHeaderValue)httpContext.Items["AcceptHeaderMediaType"];
+            return mediaType.SubTypeWithoutSuffix.EndsWith("hateoas", StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        private List<Entity> ShapedData(IEnumerable<BookDto> booksDto, string fields)
+        {
+            return _dataShaper.ShapeData(booksDto, fields).Select(s => s.Entity).ToList();
+        }
+
     }
 }
+
