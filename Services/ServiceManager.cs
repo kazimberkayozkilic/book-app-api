4a05a5a2e40d6144b437af4692d2fd2113839e31
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-09 17:11:49 +0300

    refactor Updated ServiceManager and BookManager to support link creation and data shaping. - ServiceManager` has been modified to set the `BookManager` constructor by removing `IDataShaper<BookDto>` and adding `IBookLinks`. - Updated `BookManager` to include support for `IBookLinks` to handle HATEOAS link generation. - Modified `BookManager.GetAllBooksAsync` to work with `LinkParameters` for link generation and metadata handling. - Refactored constructor dependencies in both `ServiceManager` and `BookManager` for improved modularity.

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -15,7 +15,7 @@ namespace Services
         private readonly Lazy<IBookService> _bookService;
         public ServiceManager(IRepositoryManager repositoryManager, ILoggerService logger, IMapper mapper, IDataShaper<BookDto> shaper)
         {
-            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper, shaper));
+            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, logger, mapper));
         }
 
         public IBookService BookService => _bookService.Value;
