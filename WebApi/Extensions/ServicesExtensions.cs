66e34e00bb06a30b48db85adc0afb7e38a136379
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-13 02:55:30 +0300

    feat: Added API versioning configuration. - Added `ConfigureVersioning` method to `ServiceExtensions.cs` to set API versioning.

--- WebApi/Extensions/ServicesExtensions.cs
+++ WebApi/Extensions/ServicesExtensions.cs
@@ -75,6 +75,16 @@ namespace WebApi.Extensions
             });
         }
 
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+        }
+
 
     }
 }
