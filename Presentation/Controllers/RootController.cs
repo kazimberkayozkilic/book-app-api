c2bd88bae0e2a123d0d6346056df808695c0b005
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-12 18:34:05 +0300

    feat: Implemented the base structure for RootController with the GetRoot endpoint. - Updated `RootController` to include `LinkGenerator` for creating dynamic links. - Added `GetRoot` endpoint with support for checking `Accept` header for `Application/vnd.bsstore.apiroot` media type.

--- Presentation/Controllers/RootController.cs
+++ Presentation/Controllers/RootController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,9 +8,26 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
+    [ApiController]
+    [Route("api")]
     public class RootController : ControllerBase
     {
 
+        private readonly LinkGenerator _linkGenerator;
 
+        public RootController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet(Name = "GetRoot")]
+        public async Task<IActionResult> GetRoot([FromHeader(Name = "Accept")] string MediaType)
+        {
+            if(MediaType.Contains("application/vnd.bsstore.apiroot"))
+            {
+
+            }
+            return NoContent();
+        }
     }
 }
