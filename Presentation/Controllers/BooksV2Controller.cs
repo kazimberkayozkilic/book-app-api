4a1564cc732d6f756007455437944fed81b85691
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-13 03:01:22 +0300

    feat: Added API versioning attributes to BooksV2Controller. - Added `[ApiVersion(“2.0”)]` to specify API version 2.0 for this controller. - Added `[ApiController]` for automatic model binding and validation. - Updated `[Route(“api/books”)]` to define the route for the controller.

--- Presentation/Controllers/BooksV2Controller.cs
+++ Presentation/Controllers/BooksV2Controller.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace Presentation.Controllers
 {
+    [ApiVersion("2.0")]
+    [ApiController]
+    [Route("api/books")]
     public class BooksV2Controller: ControllerBase
     {
     }
