2e18e68e47fe057c13ee0661ba7b640734ecbec4
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-16 01:25:38 +0300

    feat: Implemented the AuthenticationManager class with user registration. - Added `AuthenticationManager` class that implements the `IAuthenticationService` interface. - Dependencies included: `ILoggerService`, `IMapper`, `UserManager<User>` and `IConfiguration`.

---
+++ Services/AuthenticationManager.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class AuthenticationManager : IAuthenticationService
+    {
+        private readonly ILoggerService _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationManager(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
