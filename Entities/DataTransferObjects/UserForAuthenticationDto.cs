2b904a4adff66bd8cf9927e9db061fb21e3ec006
“kazimberkayozkilic” <berkayozkilic@gmail.com>
2025-01-17 03:59:30 +0300

    feat: Implemented user authentication in AuthenticationManager. - Added a `_user` custom field to hold the user during validation. - Implemented `ValidateUser` method to check user credentials using `UserManager`.

--- Entities/DataTransferObjects/UserForAuthenticationDto.cs
+++ Entities/DataTransferObjects/UserForAuthenticationDto.cs
@@ -9,8 +9,8 @@ namespace Entities.DataTransferObjects
 {
     public record UserForAuthenticationDto
     {
-        [Required(ErrorMessage = "Email is required")]
-        public string? Email { get; init; }
+        [Required(ErrorMessage = "User name is required")]
+        public string? UserName { get; init; }
 
         [Required(ErrorMessage = "Password is required")]
         public string? Password { get; init; }
--- Services/AuthenticationManager.cs
+++ Services/AuthenticationManager.cs
@@ -19,6 +19,7 @@ namespace Services
         private readonly UserManager<User> _userManager;
         private readonly IConfiguration _configuration;
 
+        private User? _user;
         public AuthenticationManager(ILoggerService logger, IMapper mapper, UserManager<User> userManager, IConfiguration configuration)
         {
             _logger = logger;
@@ -30,14 +31,26 @@ namespace Services
         public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistrationDto)
         {
             var user = _mapper.Map<User>(userForRegistrationDto);
-            
+
             var result = await _userManager.CreateAsync(user, userForRegistrationDto.Password);
-            
+
             if (result.Succeeded)
             {
                 await _userManager.AddToRolesAsync(user, userForRegistrationDto.Roles);
             }
             return result;
         }
+
+        public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuthenticationDto)
+        {
+            _user = await _userManager.FindByNameAsync(userForAuthenticationDto.UserName);
+            var result = (_user != null && await _userManager.CheckPasswordAsync(_user, userForAuthenticationDto.Password));
+            if (!result)
+            {
+                _logger.LogError($"Invalid attempt to log in for user: {userForAuthenticationDto.UserName} ");
+            }
+            return result;
+        }
     }
 }
+
